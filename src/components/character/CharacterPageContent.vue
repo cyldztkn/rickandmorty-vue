64a54a5629062188d54578528e560861b5384551
Cagkan <99582229+cyldztkn@users.noreply.github.com>
2024-06-20 01:24:46 +0300

    "comment line edited"

--- src/components/character/CharacterPageContent.vue
+++ src/components/character/CharacterPageContent.vue
@@ -4,7 +4,7 @@ import CharacterImage from '@/components/character/CharacterImage.vue';
 import CharacterInfo from '@/components/character/CharacterInfo.vue';
 import LoadingScreen from '../generic/LoadingScreen.vue';
 
-const path = window.location.pathname.split("/").slice(-1)[0]; // path'in son öğesini alın
+const path = window.location.pathname.split("/").slice(-1)[0]; 
 let data = reactive({
     content: null
 });
